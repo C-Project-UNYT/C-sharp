c6b0160b3ed732e770cce88aa970b6798b17ff01
SaraHoxha <sarahoxha99@gmail.com>
2021-06-14 18:32:41 +0200

    UPDATED CLASSES
    CHANGES: 1.klasat prof,student,admin i vendosa ne klasa me vete per clarity. 2. added username dhe password si fields te Person sepse nuk mudn te bejme dot extend dy klasa.

---
+++ Admin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT
+{
+    class Admin : Login
+    {
+        public bool usernameValid()
+        {
+            return false;
+        }
+
+        public bool passwordValid()
+        {
+            return false;
+        }
+
+        public void addProfessor()
+        {
+
+        }
+        public void removeProfessor()
+        {
+
+        }
+        public void addStudent()
+        {
+
+        }
+        public void removeStudent()
+        {
+
+        }
+        public void addCourse()
+        {
+
+        }
+        public void removeCourse()
+        {
+
+        }
+    }
+
+}
--- Form1.cs
+++ Form1.cs
@@ -17,101 +17,18 @@ namespace PROJECT
             InitializeComponent();
         }
 
-    }
-
-    interface Login //take a look... //ms duhet e 1 klase abstract login info
-    {
-        bool usernameValid();
-        bool passwordValid();
-    }
-
-    abstract class Person
-    {
-        private string name;
-        private string surname;
-
-        public Person(string name, string surname)
-        {
-            this.Name = name;
-            this.Surname = surname;
-        }
-
-        public string Name { get => name; set => name = value; }
-        public string Surname { get => surname; set => surname = value; }
-    }
-
-    class Admin : Login
-    {
-        public bool usernameValid()
-        {
-            return false;
-        }
-
-        public bool passwordValid()
-        {
-            return false;
-        }
-
-        public void addProfessor()
-        {
-
-        }
-        public void removeProfessor()
-        {
-
-        }
-        public void addStudent()
-        {
-
-        }
-        public void removeStudent()
-        {
-
-        }
-        public void addCourse()
-        {
-
-        }
-        public void removeCourse()
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
     }
 
-    class Professor : Person, Login
-    {
+}
 
-        public Professor(string name, string surname) : base(name, surname)
-        {
+    
 
-        }
+    
+    
 
-        public bool passwordValid()
-        {
-            return false;
-        }
-
-        public bool usernameValid()
-        {
-            return false;
-        }
-    }
-
-    class Student: Person, Login
-    {
-        public Student (string name, string surname) : base(name, surname)
-        {
-
-        }
-
-        public bool passwordValid()
-        {
-            return false;
-        }
-
-        public bool usernameValid()
-        {
-            return false;
-        }
-    }
+    
 }
