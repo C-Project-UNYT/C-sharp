a47b9d953cda97f5855ee1d1f5c1ac97d00c9363
estersanxhaku1 <estersanxhaku1@gmail.com>
2021-06-16 15:00:06 +0200

    coursesFiles<3
    nqs ka ndonje problem me thoni

--- Courses.cs
+++ Courses.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,13 +15,97 @@ namespace PROJECT
         string professor;
         int noOfStudents;
 
-        public Courses(string subject, string time, int credits, string professor, int noOfStudents)
+        public string Subject
         {
-            this.subject = subject;
-            this.time = time;
-            this.credits = credits;
-            this.professor = professor;
-            this.noOfStudents = noOfStudents;
+            set
+            {
+                subject = value;
+            }
+            get
+            {
+                return subject;
+            }
+        }
+        public string Time
+        {
+            set
+            {
+                time = value;
+            }
+            get
+            {
+                return time;
+            }
+        }
+        public int Credits
+        {
+            set
+            {
+                credits = value;
+            }
+            get
+            {
+                return credits;
+            }
+        }
+        public string Professor
+        {
+            set
+            {
+                professor = value;
+            }
+            get
+            {
+                return professor;
+            }
+        }
+        public int NoOfStudents
+        {
+            set
+            {
+                noOfStudents = value;
+            }
+            get
+            {
+                return noOfStudents;
+            }
+        }
+     
+        public Courses(string subject, string time,int credits, string professor, int noOfStudents)
+        {
+            this.Subject = subject;
+            this.Time = time;
+            this.Credits = credits;
+            this.Professor = professor;
+            this.NoOfStudents = noOfStudents;
+        }
+        public List<Courses> getInfoFromFile()
+        {
+            List<Courses> list = new List<Courses>();
+
+            StreamReader reader = new StreamReader("CoursesFile.txt");
+
+            while (!reader.EndOfStream)
+            {
+
+                var line = reader.ReadLine();
+                var data = line.Split(",");
+
+
+                Courses course = new Courses(data[0], data[1], credits, data[3],noOfStudents);
+
+                for (int i = 5; i < data.Length; i++)
+                    course.Add(data[i]);
+
+                list.Add(course);
+            }
+            reader.Close();
+
+            return list;
+        }
+        private void Add(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
