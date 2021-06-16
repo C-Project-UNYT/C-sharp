7c9f6725f574502f4fddf91ceed83203b804f642
Endi_Trico <enditrico@gmail.com>
2021-06-16 20:00:32 +0200

    uff

---
+++ Form_Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT
+{
+    public partial class Form_Student : Form
+    {
+        public Form_Student()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Student_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
